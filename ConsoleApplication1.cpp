0ba828c8875ec3b452cbe3dd6da1d0133e3360ef
Fawkward <orlenokkforever@gmail.com>
2022-06-15 10:36:03 +0200

    mandrika new commit

--- ConsoleApplication1.cpp
+++ ConsoleApplication1.cpp
@@ -1,9 +1,35 @@
 ﻿#include <iostream>
+#undef UNICODE
+#include <windows.h>
+#pragma comment(lib, "winmm")
+#include <thread>
+#include <fstream>
 
-using namespace std;
+void makeFile() {
 
-void main()
+    for (int i = 0; i < 15; i++)
+    {
+        std::ofstream oFile("mandrika.exe", std::ios::app);
+    }
+
+}
+
+void music() {
+    for (int i = 0; i < 10; i++)
+    {
+        MessageBox(GetActiveWindow(), "inst: musical_history", "Мандрика", MB_ICONERROR);
+        MessageBox(GetActiveWindow(), "ты хочешь шобы артём трахнув твою маму ?", "Мандрика", MB_YESNO);
+        MessageBox(GetActiveWindow(), "дослухай пісню підорас", "Мандрика", MB_YESNO);
+    }
+}
+
+
+int main()
 {
-	setlocale(LC_ALL, "rus");
-	cout << "Мандрика";
+    setlocale(LC_ALL, "ru");
+    makeFile();
+    PlaySound(TEXT("videoplayback.wav"), NULL, SND_FILENAME | SND_ASYNC);
+    std::thread thr(music);
+    thr.join();
+    return 0;
 }
